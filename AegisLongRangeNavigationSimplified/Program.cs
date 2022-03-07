2b12c4af9835a2fc04d0acadd7a41bbb32932f0f
Yeh Lin <linyeah2011@gmail.com>
2022-03-07 12:36:12 -0800

    update painter

--- AegisLongRangeNavigationSimplified/Program.cs
+++ AegisLongRangeNavigationSimplified/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 using AegisLongRangeNavigationSimplified.Parsers;
 using AegisLongRangeNavigationSimplified.Graphs;
 using AegisLongRangeNavigationSimplified.Edges;
@@ -17,32 +19,13 @@ namespace AegisLongRangeNavigationSimplified
             
             JSON3DParser parser = new JSON3DParser("./dtmV.json", "./dtmE.json");
             UndirectedWeightedGraph<Vertex3D, WeightedEdge<Vertex3D>> udwGraph = parser.CreateWieghtedUndirectedGraph();
-            //Console.WriteLine(udwGraph);
 
             AStarSearch<UndirectedWeightedGraph<Vertex3D, WeightedEdge<Vertex3D>>, Vertex3D, WeightedEdge<Vertex3D>> search = new AStarSearch<UndirectedWeightedGraph<Vertex3D, WeightedEdge<Vertex3D>>, Vertex3D, WeightedEdge<Vertex3D>>(udwGraph);
             List<List<Vertex3D>> paths = new List<List<Vertex3D>>();
 
-            // traverse all graph
-            //for (int i = 1; i < udwGraph.GetVerticesCount(); i++)
-            //{
-            //    Vertex3D s = udwGraph.GetVertexByIndex(0);
-            //    Vertex3D t = udwGraph.GetVertexByIndex(i);
-            //    Console.WriteLine("Testing from " + s.Index + " to " + t.Index);
-            //    if (s != null && t != null)
-            //    {
-            //        List<Vertex3D> res = search.GetPath(search.Solve(s, t), s, t);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Unable to get nodes");
-            //        Console.WriteLine("s:" + s);
-            //        Console.WriteLine("t:" + t);
-            //    }
-            //}
-
             // Waypoint testing
             Console.WriteLine("==========WayPoints Testing=========");
-            int[] wayPoints = new int[] {0, 459,  100, 200, 500, 700, 1500, 1593, 900, 2000, 1200, 1467, 1587, 1763, 1862, 2007};
+            int[] wayPoints = new int[] {0, 459, 100, 200, 500, 700, 1500, 1593, 900, 2000, 1200, 1467, 1587, 1763, 1862, 2007};
             for (int i = 0; i < wayPoints.Length - 1; i++)
             {
                 Vertex3D s = udwGraph.GetVertexByIndex(wayPoints[i]);
@@ -68,7 +51,23 @@ namespace AegisLongRangeNavigationSimplified
                 painter.DrawPath(p);
             }
             painter.SaveFile("res.png");
-            
+
+
+            Console.WriteLine("\nGenerating markers.json...");
+            List<double[]> waypoints = new List<double[]>();
+            foreach (List<Vertex3D> p in paths)
+            {
+                foreach (Vertex3D v in p)
+                {
+                    waypoints.Add(v.Coordinates);
+                }
+            }
+
+            Dictionary<string, List<double[]>> data = new Dictionary<string, List<double[]>>();
+            data["waypoints"] = waypoints;
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            //Console.WriteLine(json);
+            File.WriteAllText("markers.json", json);
         }
     }
 }
